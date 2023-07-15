3600d35026b6d5c0571ae6defbebb3563d14a537
alaziiammar <alaziiammar@gmail.com>
2023-07-15 11:39:17 +0300

    fix redirect login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -53,7 +53,7 @@ const login = () => {
       localStorage.setItem('accessToken', JSON.stringify(accessToken))
 
       // Redirect to `to` query if exist or redirect to index route
-      router.replace(route.query.to ? String(route.query.to) : '/')
+      router.push(route.query.to ? String(route.query.to) : '/')
     })
     .catch(e => {
       loading.value = false
